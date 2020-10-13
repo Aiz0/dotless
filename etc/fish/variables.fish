67fbfab68d094ecd8712784e9f853539d843037b
Aiz <dev@aiz.moe>
2020-10-13 18:05:20 +0200

    export SXHKD_SHELL in fish instead of xinitrc

--- etc/fish/variables.fish
+++ etc/fish/variables.fish
@@ -4,4 +4,6 @@ set -gx EDITOR "nvim"
 
 set -gx PATH $PATH "$HOME/bin"
 
- set -gx DOTBARE_DIR "$XDG_STATE_HOME/dotbare"
+set -gx DOTBARE_DIR "$XDG_STATE_HOME/dotbare"
+
+set -gx SXHKD_SHELL "/usr/bin/sh"
