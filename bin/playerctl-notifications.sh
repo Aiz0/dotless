d8ff38d527ac6ab1b2da33adc0c392ec5d6fafee
Aiz <dev@aiz.moe>
2020-12-29 05:25:50 +0100

    change player from spotify to spotifyd

--- bin/playerctl-notifications.sh
+++ bin/playerctl-notifications.sh
@@ -1,9 +1,9 @@
 #!/bin/fish
-
-set playerStatus (playerctl status -p spotify)
+set musicPlayer spotifyd
+set playerStatus (playerctl status -p $musicPlayer)
 
 if test (test "$playerStatus" = "Playing") -o (test "$playerStatus" = "Paused")
-    dunstify -a showMusic $playerStatus (playerctl metadata title -p spotify)\n(playerctl metadata artist -p spotify) -r 5
+    dunstify -a showMusic $playerStatus (playerctl metadata title -p $musicPlayer)\n(playerctl metadata artist -p $musicPlayer) -r 5
    else
     dunstify -a showMusic $playerStatus
 end
