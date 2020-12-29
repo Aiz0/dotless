7ce033ac95b233c3b089f2ea998fee9f8ad5ce61
Aiz <dev@aiz.moe>
2020-12-29 06:05:55 +0100

    add colemak vi keybinds to fish

---
+++ etc/fish/functions/fish_colemak_vi_key_bindings.fish
@@ -0,0 +1,28 @@
+function fish_colemak_vi_key_bindings
+    # set qwerty vi keybinds
+    fish_vi_key_bindings
+
+    # Move forward. Moving backwards is unchanged
+    bind -s --preset -M default i forward-char
+
+    # insert
+    bind -s --preset -m insert u repaint-mode
+    bind -s --preset -m insert U beginning-of-line repaint-mode
+
+
+    bind -s --preset e up-or-search
+    bind -s --preset n down-or-search
+
+    bind -s --preset N end-of-line delete-char
+    bind -s --preset E 'man (commandline -t) 2>/dev/null; or echo -n \a'
+
+    #
+    # visual mode
+    #
+
+    bind -s --preset -M visual i forward-char
+
+    bind -s --preset -M visual e up-line
+    bind -s --preset -M visual n down-line
+
+end
---
+++ etc/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,3 @@
+function fish_user_key_bindings
+    fish_colemak_vi_key_bindings
+end
