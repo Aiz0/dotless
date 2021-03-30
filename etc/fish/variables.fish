c16be979417f6a32d05d359927147de1be53c619
Aiz <dev@aiz.moe>
2021-03-30 06:36:31 +0200

    feat: use fish_add_path to add paths

--- etc/fish/variables.fish
+++ etc/fish/variables.fish
@@ -4,10 +4,11 @@ set -gx EDITOR "nvim"
 
 set -gx QT_STYLE_OVERRIDE kvantum
 
-set -gx PATH $PATH "$HOME/bin" "$XDG_STATE_HOME/cargo/bin"
-
 set -gx DOTBARE_DIR "$XDG_STATE_HOME/dotbare"
 
 set -gx SXHKD_SHELL "/usr/bin/sh"
 
 set -gx MUSIC_PLAYER spotifyd
+
+# PATH
+fish_add_path "$HOME/bin" "$XDG_STATE_HOME/cargo/bin"
