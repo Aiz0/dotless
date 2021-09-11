a97f40e8b4971508041040114ea34bc136a0ee7c
Aiz <dev@aiz.moe>
2021-09-11 18:58:41 +0200

    fix: fish only load abbr and alias in interactive
    This avoids problemes when using aliased commands such as ls and cd in
    scripts.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,9 +1,11 @@
-# Zoxide needs to generate shell configuration
-# before any zoxide related aliases
-zoxide init fish | source
+if status --is-interactive
+    # Zoxide needs to generate shell configuration
+    # before any zoxide related aliases
+    zoxide init fish | source
+    source $__fish_config_dir/abbr.fish
+    source $__fish_config_dir/aliases.fish
+end
 
-source $__fish_config_dir/abbr.fish
-source $__fish_config_dir/aliases.fish
 source $__fish_config_dir/variables.fish
 source $__fish_config_dir/login.fish
 
