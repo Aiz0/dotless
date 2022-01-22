a90b7e2defb3824d3a92c8ebffd3bb5c1ca48236
Aiz <dev@aiz.moe>
2022-01-22 01:09:36 +0100

    feat: do variable declaration in environment.d instead of .pam_environment
    inspired by ayekat
    
    XDG_CONFIG_HOME is still set in ~/.pam_environment due to fish needing it to find its config files.
    
    XDG_DATA_HOME is also set there due to fish creating files there.
    I will look into fixing that so XDG_DATA_HOME doesn't need to be set there.

---
+++ etc/fish/conf.d/00-environment.fish
@@ -0,0 +1,25 @@
+if status is-login
+    for envfile in $XDG_CONFIG_HOME/environment.d/*.conf
+        set linenumber 0
+        for line in (cat $envfile)
+            set linenumber (math $linenumber + 1)
+            switch $line
+                # empty lines
+                case ''
+                    continue
+                # commented lines
+                case '#*'
+                    continue
+                # variable lines
+                # currently assumes have valid variable names
+                case '*=*'
+                    set line (string replace = ' ' $line)
+                    eval set -gx $line
+                # all other lines
+                case "*"
+                    printf '%s:%d: Ignoring incorrectly formatted line: %s\n' \
+                   "$envfile" $linenumber "$line"
+            end
+        end
+    end
+end
