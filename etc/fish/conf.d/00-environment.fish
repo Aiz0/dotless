0408d8d1873edc54523a7ee1d5835c4785f2d02f
Aiz <dev@aiz.moe>
2022-02-11 00:00:02 +0100

    feat(fish): import variables via systemd show-environment

--- etc/fish/conf.d/00-environment.fish
+++ etc/fish/conf.d/00-environment.fish
@@ -1,23 +1,4 @@
-for envfile in $XDG_CONFIG_HOME/environment.d/*.conf
-    set linenumber 0
-    for line in (cat $envfile)
-        set linenumber (math $linenumber + 1)
-        switch $line
-            # empty lines
-            case ''
-                continue
-            # commented lines
-            case '#*'
-                continue
-            # variable lines
-            # currently assumes have valid variable names
-            case '*=*'
-                set line (string replace = ' ' $line)
-                eval set -gx $line
-            # all other lines
-            case "*"
-                printf '%s:%d: Ignoring incorrectly formatted line: %s\n' \
-               "$envfile" $linenumber "$line"
-        end
-    end
+for line in (systemctl --user show-environment)
+    string match -rq '(?<key>.*?)=(?<value>.*)' $line
+    set -gx $key $value
 end
