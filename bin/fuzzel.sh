9891aef74de6e4151e7d461452621c419ad2b716
Aiz <dev@aiz.moe>
2022-02-16 03:34:49 +0100

    feat(fuzzel): wrapper script for fuzzel

---
+++ bin/fuzzel.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+exec fuzzel --font monospace:20 \
+            --lines 5 \
+            --width 50 \
+            --horizontal-pad 20 \
+            --background 282C33FF \
+            --text-color f2f2f4ff \
+            --match-color 71c6ffff \
+            --selection-color 2e343fff \
+            --selection-text-color f2f2f4ff \
+            --border-width 0 \
+            --border-radius 0
--- etc/river/conf.d/50-bindings-spawn.fish
+++ etc/river/conf.d/50-bindings-spawn.fish
@@ -10,7 +10,7 @@ riverctl map normal Super+Shift P spawn "ff --private-window"
 riverctl map normal Super+Alt P spawn "ff -P 'default'"
 
 # Launcher
-riverctl map normal Super Space spawn "fuzzel --font monospace:20 --lines 5 --width 50 --horizontal-pad 20 --background 282C33FF --text-color f2f2f4ff  --match-color 71c6ffff --selection-color 2e343fff --selection-text-color f2f2f4ff --border-width 0 --border-radius 0"
+riverctl map normal Super Space spawn "fuzzel.sh"
 
 
 # Lock
