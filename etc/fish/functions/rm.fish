983afb3158b85cc3dc9ecc977a9d52f01ff1a65d
Aiz <dev@aiz.moe>
2022-02-27 23:09:22 +0100

    fix(fish): only disable rm in interactive shell
    Before switching from alias to functions aliases where loaded only in interactive.
    this reintroduces that by only loading rm function in interactive shell

--- etc/fish/functions/rm.fish
+++ etc/fish/functions/rm.fish
@@ -1,6 +1,8 @@
 # Disable rm so i don't accidentally use it
-function rm
-    set_color red
-    echo "Use trash-cli instead!"
-    echo "rm has been aliased to remove."
+if status is-interactive
+    function rm
+        set_color red
+        echo "Use trash-cli instead!"
+        echo "rm has been aliased to remove."
+    end
 end
