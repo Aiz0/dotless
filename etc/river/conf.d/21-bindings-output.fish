73208333cc66e2a3ad832b39fa00819925ab2255
Aiz <dev@aiz.moe>
2022-05-19 19:36:36 +0200

    feat(river): Use L+U to focus/send to outputs
    Locking and powermenu now mapped to Escape.

--- etc/river/conf.d/21-bindings-output.fish
+++ etc/river/conf.d/21-bindings-output.fish
@@ -1,9 +1,9 @@
 #!/bin/fish
 
 # Super+Period and Mod+Comma to focus the next/previous output
-riverctl map normal Super Period focus-output next
-riverctl map normal Super Comma focus-output previous
+riverctl map normal Super L focus-output next
+riverctl map normal Super U focus-output previous
 
 # Super+Shift+{Period,Comma} to send the focused view to the next/previous output
-riverctl map normal Super+Shift Period send-to-output next
-riverctl map normal Super+Shift Comma send-to-output previous
+riverctl map normal Super+Shift L send-to-output next
+riverctl map normal Super+Shift U send-to-output previous
--- etc/river/conf.d/50-bindings-spawn.fish
+++ etc/river/conf.d/50-bindings-spawn.fish
@@ -14,8 +14,11 @@ riverctl map normal Super Space spawn "fuzzel.sh"
 
 
 # Lock
-riverctl map normal Super L spawn "swaylock -f"
+riverctl map normal Super Escape spawn "swaylock -f"
+# Powermenu
+riverctl map normal Super+Shift Escape spawn "nwg-bar -i 128"
 
 # Screenshot
 riverctl map normal Super D spawn "wl-screenshot.sh --optimize"
-riverctl map normal Super C spawn "wl-screenshot.sh --select --copy --optimize"
+riverctl map normal Super C spawn "wl-screenshot.sh --select=area --copy --optimize"
+riverctl map normal Super+Shift C spawn "wl-screenshot.sh --select=output --optimize"
