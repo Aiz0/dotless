6cdc57956ea1545da9126471361615ea131bc9d2
Aiz <dev@aiz.moe>
2022-05-24 15:39:31 +0200

    fix(river): update output bindings comments

--- etc/river/conf.d/21-bindings-output.fish
+++ etc/river/conf.d/21-bindings-output.fish
@@ -1,9 +1,9 @@
 #!/bin/fish
 
-# Super+Period and Mod+Comma to focus the next/previous output
+# Super+Period and L+U to focus the next/previous output
 riverctl map normal Super L focus-output next
 riverctl map normal Super U focus-output previous
 
-# Super+Shift+{Period,Comma} to send the focused view to the next/previous output
+# Super+Shift+{L,U} to send the focused view to the next/previous output
 riverctl map normal Super+Shift L send-to-output next
 riverctl map normal Super+Shift U send-to-output previous
