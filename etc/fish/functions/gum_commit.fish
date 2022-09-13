d179f6ac8a1ecc6276fd4af2d51262b6cb89555f
Aiz <dev@aiz.moe>
2022-09-13 16:30:02 +0200

    fix(fish script): fix empty scoop in conventional commits script
    () would get added even when scope was empty this fixes that.

--- etc/fish/functions/gum_commit.fish
+++ etc/fish/functions/gum_commit.fish
@@ -5,8 +5,8 @@ function gum_commit
     set SCOPE (gum input --placeholder "scope")
 
     # Since the scope is optional, wrap it in parentheses if it has a value.
-    test -n $SCOPE && set SCOPE "($SCOPE)"
-
+    test -n "$SCOPE" && set SCOPE "($SCOPE)"
+    
     # Pre-populate the input with the type(scope): so that the user may change it
     set SUMMARY (gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
     set DESCRIPTION (gum write --placeholder "Details of this change (CTRL+D to finish)")
