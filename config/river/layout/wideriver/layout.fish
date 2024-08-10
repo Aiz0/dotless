a7e7cecca943145faa14b67f2beed3e68a9525fa
Aiz <dev@aiz.moe>
2024-08-10 03:06:10 +0200

    fix(river): start wideriver with river-session
    it started before it before and didn't work

--- config/river/layout/wideriver/layout.fish
+++ config/river/layout/wideriver/layout.fish
@@ -1,6 +1,5 @@
 #!/bin/fish
 set layout wideriver
 riverctl default-layout $layout
-systemctl --user start $layout
 
-riverctl map normal Super+Shift Space send-layout-cmd wideriver "--layout-toggle"
+riverctl map normal Super+Shift Space send-layout-cmd $layout "--layout-toggle"
