4dfd1d769afdbaafd6e580152d0a2ac8a65233c1
Aiz <dev@aiz.moe>
2024-08-10 03:20:38 +0200

    feat: switch to swayosd for volume indicator popup

--- bin/notify-volume.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/fish
-# Gets the current volume in % from amixer and creates a notification with dunstify
-
-# Gets the Right volume
-set volume (amixer sget Master | grep 'Right:' | awk -F '[][]' '{ print $2 }')
-
-notify-send Volume $volume -r 6
--- config/river/bindings.fish
+++ config/river/bindings.fish
@@ -92,9 +92,9 @@ riverctl map normal Super+Shift 0 set-view-tags $all_tags
 # not have a modifier
 for mode in normal locked
     # Control pulse audio volume with pamixer (https://github.com/cdemoulins/pamixer)
-    riverctl map $mode None XF86AudioRaiseVolume  spawn "pamixer -i 5; notify-volume.sh"
-    riverctl map $mode None XF86AudioLowerVolume  spawn "pamixer -d 5; notify-volume.sh"
-    riverctl map $mode None XF86AudioMute         spawn "pamixer --toggle-mute"
+    riverctl map $mode None XF86AudioRaiseVolume  spawn "swayosd-client --output-volume raise"
+    riverctl map $mode None XF86AudioLowerVolume  spawn "swayosd-client --output-volume lower"
+    riverctl map $mode None XF86AudioMute         spawn "swayosd-client --output-volume mute-toggle"
 
     # Control MPRIS aware media players with playerctl (https://github.com/altdesktop/playerctl)
     riverctl map $mode None XF86AudioMedia spawn "playerctl play-pause; sleep 0.15; playerctl-notifications.sh &"
