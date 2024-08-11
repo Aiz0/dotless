75ec9ce11a8d135654c5e75a9e867aef8f27e1bb
Aiz <dev@aiz.moe>
2024-08-12 01:51:25 +0200

    chore(ags): rm powermenu stuff

--- config/ags/config.js
+++ config/ags/config.js
@@ -2,13 +2,11 @@ import { forMonitors } from "./js/utils.js";
 import options from "./js/options.js";
 import Sidebar from "./js/sidebar/config.js";
 import Notifications from "./js/notifications/config.js";
-import PowerMenu from "./js/powermenu/config.js";
 import DashBoard from "./js/dashboard/config.js";
 
 const windows = () => [
   forMonitors(Sidebar),
   forMonitors(Notifications),
-  PowerMenu(),
   DashBoard(),
 ];
 
@@ -19,7 +17,7 @@ Utils.exec(
   sed -i 's/:active/__TEMP_ACTIVE__/' ${options.path.css};
   bunx postcss ${options.path.css} -r;
   sed -i 's/__TEMP_HOVER__/:hover/' ${options.path.css};
-  sed -i 's/__TEMP_ACTIVE__/:active/' ${options.path.css};"`
+  sed -i 's/__TEMP_ACTIVE__/:active/' ${options.path.css};"`,
 );
 
 App.config({
