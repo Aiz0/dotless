1b2be23c9357643ef274f50a078751f49c32d7f9
Aiz <dev@aiz.moe>
2024-08-15 01:29:20 +0200

    feat(ags): notification icon can change and button will be selected whin swaync is open

--- config/ags/js/sidebar/notifications.js
+++ config/ags/js/sidebar/notifications.js
@@ -1,11 +1,25 @@
 import IconButton from "../widgets/icon_button.js";
 import Icons from "../icons.js";
+import { swaync } from "../variables.js";
 
 export default () =>
   Widget.Box({
     hpack: "center",
     child: IconButton({
       label: Icons.notifcations.normal,
-      on_clicked: () => Utils.exec("swaync-client -t"),
+      on_clicked: () => Utils.exec("swaync-client --toggle-panel"),
+      onSecondaryClickRelease: () => Utils.exec("swaync-client --toggle-dnd"),
+    }).hook(swaync, (self) => {
+      if (swaync.value.dnd) {
+        self.label = Icons.notifcations.dnd;
+      } else if (swaync.value.count > 0) {
+        self.label = Icons.notifcations.badge;
+      } else {
+        self.label = Icons.notifcations.normal;
+      }
+      self.attribute = {
+        ...self.attribute,
+        selected: swaync.value.visible || false,
+      };
     }),
   });
