e17c346b0643baf3863e899dd8b388753f43997b
Aiz <dev@aiz.moe>
2024-08-15 00:40:20 +0200

    fix(ags): put powerbutton in a box so it becomes circular

--- config/ags/js/sidebar/power_menu.js
+++ config/ags/js/sidebar/power_menu.js
@@ -2,7 +2,10 @@ import Icons from "../icons.js";
 import IconButton from "../widgets/icon_button.js";
 
 export default () =>
-  IconButton({
-    label: Icons.powermenu.shutdown,
-    on_clicked: () => Utils.exec("wlogout"),
+  Widget.Box({
+    hpack: "center",
+    child: IconButton({
+      label: Icons.powermenu.shutdown,
+      on_clicked: () => Utils.exec("wlogout"),
+    }),
   });
