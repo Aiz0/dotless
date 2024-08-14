4abd0b3fd9a8ecfc72a25c11a10494a9040c1dc6
Aiz <dev@aiz.moe>
2024-08-15 00:38:39 +0200

    chore(ags): icons.js format

--- config/ags/js/icons.js
+++ config/ags/js/icons.js
@@ -1,42 +1,41 @@
-import App from 'resource:///com/github/Aylur/ags/app.js';
+import App from "resource:///com/github/Aylur/ags/app.js";
 
-const icons = App.configDir + '/assets/icons/';
+const icons = App.configDir + "/assets/icons/";
 
 export default {
-    avatar: {
-        square: icons + 'avatar.png',
-        round: icons + 'avatar-round.png',
+  avatar: {
+    square: icons + "avatar.png",
+    round: icons + "avatar-round.png",
+  },
+  packages: "󰓦",
+  protonvpn: {
+    sign: icons + "proton-vpn-sign.svg",
+    connected: icons + "proton-vpn-state-connected.svg",
+    disconnected: icons + "proton-vpn-state-disconnected.svg",
+  },
+  system: {
+    cpu: "󰍛",
+    ram: "󰞯",
+    gpu: "󰢮",
+    disk: "󰋊",
+  },
+  powermenu: {
+    shutdown: "󰐥",
+    sleep: "󰤄",
+    logout: "󰍃",
+    reboot: "󰜉",
+    lock: "󰌾",
+  },
+  audio: {
+    volume: {
+      high: "󰕾",
+      medium: "󰖀",
+      low: "󰕿",
+      muted: "󰝟",
     },
-    packages: '󰓦',
-    protonvpn: {
-        sign: icons + 'proton-vpn-sign.svg',
-        connected: icons + 'proton-vpn-state-connected.svg',
-        disconnected: icons + 'proton-vpn-state-disconnected.svg',
+    microphone: {
+      on: "󰍬",
+      off: "󰍭",
     },
-    system: {
-        cpu: '󰍛',
-        ram: '󰞯',
-        gpu: '󰢮',
-        disk: '󰋊',
-        
-    },
-    powermenu: {
-        shutdown: '󰐥',
-        sleep: '󰤄',
-        logout: '󰍃',
-        reboot: '󰜉',
-        lock: '󰌾',
-    },
-    audio: {
-        volume: {
-            high: '󰕾',
-            medium: '󰖀',
-            low: '󰕿',
-            muted: '󰝟',
-        },
-        microphone: {
-            on: '󰍬',
-            off: '󰍭',
-        }
-    },
-}
+  },
+};
