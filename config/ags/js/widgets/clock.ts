3bf110c95bc7b37b25d7baed35ff4f735e458202
Aiz <dev@aiz.moe>
2024-08-18 16:27:57 +0200

    chore(ags): clock.ts and clockprops

--- config/ags/js/widgets/clock.ts
+++ config/ags/js/widgets/clock.ts
@@ -1,11 +1,17 @@
 import GLib from "gi://GLib";
 import options from "../options.js";
+import { LabelProps } from "types/widgets/label.js";
+
+type ClockProps = LabelProps & {
+  format?: string;
+  interval?: number;
+};
 
 export default ({
   format = "%F",
   interval = options.poll.short,
   ...rest
-} = {}) =>
+}: ClockProps) =>
   Widget.Label({
     ...rest,
   }).poll(interval, (self) => {
