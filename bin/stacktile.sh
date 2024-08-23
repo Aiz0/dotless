6c09cae5f0cd36c328956d0a98fc5ed100f68757
Aiz <dev@aiz.moe>
2024-08-24 01:15:32 +0200

    chore: rm stuff

--- bin/stacktile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec stacktile  --per-tag-config \
-                --primary-sublayout rows\
-                --primary-ratio 0.5 \
-                --remainder-sublayout grid \
-                --inner-padding 4 \
-                --outer-padding 4
