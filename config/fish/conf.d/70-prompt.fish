72d2540edd26cafa891e7be3b8251120675cc265
Aiz <dev@aiz.moe>
2025-01-19 00:24:46 +0100

    feat(fish): switch to starship prompt

--- config/fish/conf.d/70-prompt.fish
+++ config/fish/conf.d/70-prompt.fish
@@ -1,5 +1,3 @@
 if status is-interactive
-    # THEME PURE #
-    set fish_function_path $__fish_config_dir/functions/theme-pure/functions/ $fish_function_path
-    source $__fish_config_dir/functions/theme-pure/conf.d/pure.fish
+    starship init fish | source
 end
