c39ecc52ab918242ea7ec1a7516a0a58d72c8bc0
Aiz <dev@aiz.moe>
2025-01-19 14:19:44 +0100

    feat(river): mappings for layout ratio

--- config/river/layout/wideriver/layout.fish
+++ config/river/layout/wideriver/layout.fish
@@ -3,3 +3,5 @@ set layout wideriver
 riverctl default-layout $layout
 
 riverctl map normal Super+Shift Space send-layout-cmd $layout "--layout-toggle"
+riverctl map normal Super m send-layout-cmd $layout '--ratio -0.05'
+riverctl map normal Super i send-layout-cmd $layout '--ratio +0.05'
